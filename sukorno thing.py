7d820a0abf4185a6cfb2ae8aede7fa6587facb2c
David B <65736434+Luminosity2002@users.noreply.github.com>
2024-02-13 11:49:09 +0000

    Update sukorno thing.py

--- sukorno thing.py
+++ sukorno thing.py
@@ -7,20 +7,57 @@ Created on Thu Feb  8 11:20:58 2024
 
 import serial
 from serial.tools.list_ports import comports
+import time
 
 for port in comports():
     print(port)
     
-ser = serial.Serial("COM6")
+ser = serial.Serial("COM5")
 if not ser.isOpen():
     ser.open()
+    print("PORT OPENED")
 print(ser)
-print("1")
-ser.write(b"RANG 20e-9")
-ser.write(b"DISP:DIG 7")
-ser.write(b"INIT")
-print("1")
-line = ser.write(b"READ?")
-print("1")
-print(line)
+
+# resets device to default settings
+print("RESET TO DEFAULT AND ZERO CHECK")
+ser.write(b"*RST\n")
+ser.write(b"SYST:ZCH ON\n")
+
+# checks device ID
+print("CHECKING DEVICE ID...")
+ser.write(b"*IDN?\n")
+time.sleep(1)
+print("Identification check:",ser.readline())
+
+time.sleep(1)
+
+# sets range to lowest
+print("SETTING RANGE AND AVG MODE")
+ser.write(b"CURR:RANG 2e-9\n")
+
+# activate averaging filter
+ser.write(b"AVER:COUN 30\n")
+ser.write(b"AVER:TCON REP\n")
+ser.write(b"AVER:ADV OFF\n")
+ser.write(b"AVER ON\n")
+
+
+
+
+# takes reading and outputs
+ser.write(b"READ?\n")
+print("TAKING READING...")
+print("READING:",ser.readline())
+
+print("YOU HAVE 5 SECONDS TO FLIP THE SWITCHES")
+for i in range(5):
+    print(i+1,"...")
+    time.sleep(1)
+    
+ser.write(b"READ?\n")
+print("TAKING READING...")
+print("READING:",ser.readline())
+
+
 ser.close()
+print("PORT CLOSED")
