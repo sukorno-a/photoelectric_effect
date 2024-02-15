be99f4bb89602a0108ec50486e0a1db4d9822b51
guavacado <118949305+sukorno-a@users.noreply.github.com>
2024-02-15 10:45:16 +0000

    changed avg filter to manual buffer calc

--- keithley_automation.py
+++ keithley_automation.py
@@ -24,11 +24,18 @@ print("RESET TO DEFAULT AND ZERO CHECK")
 ser.write(b"*RST\n")
 ser.write(b"SYST:ZCH OFF\n")
 
+
+print("CHECKING DEVICE IS OPERATIONAL...")
+ser.write(b"*OPC?\n")
+time.sleep(1)
+print("Operation complete? :",ser.readline())
+
 # checks device ID
-print("CHECKING DEVICE ID...")
+print("CHECKING DEVICE ID & OPERATION...")
 ser.write(b"*IDN?\n")
 time.sleep(1)
 print("Identification check:",ser.readline())
+
 
 time.sleep(1)
 
@@ -37,26 +44,52 @@ print("SETTING RANGE AND AVG MODE")
 ser.write(b"CURR:RANG:AUTO ON\n")
 
 # activate averaging filter
-ser.write(b"AVER:COUN 30\n")
-ser.write(b"AVER:TCON REP\n")
-ser.write(b"AVER:ADV OFF\n")
-ser.write(b"AVER ON\n")
+# ser.write(b"AVER:COUN 30\n")
+# ser.write(b"AVER:TCON REP\n")
+# ser.write(b"AVER:ADV OFF\n")
+# ser.write(b"AVER ON\n")
 
+def take_data():
+    # reset values
+    ser.write(b"TRIG:DEL 0\n")
+    ser.write(b"TRIG:COUN 10\n") # take N measurements
+    ser.write(b"*CLS\n")
+    ser.write(b"TRAC:POIN 10\n") # store N readings
+    ser.write(b"TRAC:CLE\n") # clear readings
+    ser.write(b"TRAC:FEED:CONT NEXT\n") # activate trace
+    ser.write(b"STAT:MEAS:ENAB 512\n") 
+    ser.write(b"*SRE 1\n")
 
+    # take measurements
+    ser.write(b"INIT\n")
+    ser.write(b"TRAC:DATA?\n")
+    print(ser.readline())
+
+    # output mean
+    ser.write(b"CALC3:FORM MEAN\n")
+    ser.write(b"CALC3:DATA?\n")
+    mean = ser.readline()
+    print(mean)
+
+    # output standard deviation
+    ser.write(b"CALC3:FORM SDEV\n")
+    ser.write(b"CALC3:DATA?\n")
+    std = ser.readline()
+    print(std)
+    return mean,std
 
 
 # takes reading and outputs
-
-
-ser.write(b"READ?\n")
-print("TAKING READING...")
-print("READING:",ser.readline())
+# ser.write(b"READ?\n")
+# print("TAKING READING...")
+# print("READING:",ser.readline())
 
 start = float(input("What is your starting Voltage? "))
 increment = float(input("What is your increment? "))
 readings = int(input("How many readings do you want to take? "))
 voltage = np.array([])
 current = np.array([])
+current_std = np.array([])
 
 for i in range(readings):
     new_voltage = start + i*increment
@@ -67,16 +100,21 @@ for i in range(readings):
         print(i+1,"...")
         time.sleep(1)
     
-    ser.write(b"READ?\n")
-    print("TAKING READING...")
-    reading = ser.readline()
+    # ser.write(b"READ?\n")
+    # print("TAKING READING...")
+    # reading = ser.readline()
     # print("READING:",ser.readline())
-    current = np.append(current,)
-    
+    mean,std = take_data()
+    current = np.append(current,mean)
+    current_std = np.append(current_std,std
     
 
 
 ser.close()
 print("PORT CLOSED")
+
+
+
+
 
 
