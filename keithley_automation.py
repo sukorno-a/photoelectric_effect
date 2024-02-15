3e2959d23b50c023f03217ba14f14712ba6ff8e1
David B <65736434+Luminosity2002@users.noreply.github.com>
2024-02-15 10:18:27 +0000

    Update keithley_automation.py

--- keithley_automation.py
+++ keithley_automation.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Script to automate current value reading from Keithley Picoammeter 6485
+Created on Thu Feb  8 11:20:58 2024
+
+@author: David
 """
 
 import serial
 from serial.tools.list_ports import comports
 import time
+import numpy as np
 
-# list of all connected COM ports
 for port in comports():
     print(port)
-
-# select and open picoammeter port
-ser = serial.Serial("COM5") 
+    
+ser = serial.Serial("COM5")
 if not ser.isOpen():
     ser.open()
     print("PORT OPENED")
 print(ser)
 
-# reset device to default settings and zero check
+# resets device to default settings
 print("RESET TO DEFAULT AND ZERO CHECK")
 ser.write(b"*RST\n")
 ser.write(b"SYST:ZCH OFF\n")
 
-# check device ID
+# checks device ID
 print("CHECKING DEVICE ID...")
 ser.write(b"*IDN?\n")
 time.sleep(1)
@@ -31,9 +32,9 @@ print("Identification check:",ser.readline())
 
 time.sleep(1)
 
-# set range to lowest
+# sets range to lowest
 print("SETTING RANGE AND AVG MODE")
-ser.write(b"CURR:RANG 2e-9\n")
+ser.write(b"CURR:RANG:AUTO ON\n")
 
 # activate averaging filter
 ser.write(b"AVER:COUN 30\n")
@@ -41,20 +42,41 @@ ser.write(b"AVER:TCON REP\n")
 ser.write(b"AVER:ADV OFF\n")
 ser.write(b"AVER ON\n")
 
-# take reading
+
+
+
+# takes reading and outputs
+
+
 ser.write(b"READ?\n")
 print("TAKING READING...")
 print("READING:",ser.readline())
 
-print("YOU HAVE 5 SECONDS TO FLIP THE SWITCHES")
-for i in range(5):
-    print(i+1,"...")
-    time.sleep(1)
+start = float(input("What is your starting Voltage? "))
+increment = float(input("What is your increment? "))
+readings = int(input("How many readings do you want to take? "))
+voltage = np.array([])
+current = np.array([])
 
-# take reading
-ser.write(b"READ?\n")
-print("TAKING READING...")
-print("READING:",ser.readline())
+for i in range(readings):
+    new_voltage = start + i*increment
+    voltage = np.append(voltage,new_voltage)
+    print("Set the voltage to",new_voltage)
+    print("YOU HAVE 5 SECONDS TO FLIP THE SWITCHES")
+    for i in range(5):
+        print(i+1,"...")
+        time.sleep(1)
+    
+    ser.write(b"READ?\n")
+    print("TAKING READING...")
+    reading = ser.readline()
+    # print("READING:",ser.readline())
+    current = np.append(current,)
+    
+    
+
 
 ser.close()
 print("PORT CLOSED")
+
+
